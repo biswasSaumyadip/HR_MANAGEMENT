079856b1683b26194d339cc20deee5b37edb0a3a
Assassinationrogue <saumyadip11@gmail.com>
2023-03-11 11:59:19 +0530

    Project initialized

---
+++ src/test/java/com/human_resource/hr_management/HrManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.human_resource.hr_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
