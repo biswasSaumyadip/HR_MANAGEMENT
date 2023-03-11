fcd48e530b904864fc5da276544c1c71495279fe
Assassinationrogue <saumyadip11@gmail.com>
2023-03-11 16:03:24 +0530

    Create employee should generate UUID when it's null and postMapping added for create employee

--- src/main/java/com/human_resource/hr_management/v1/controller/EmployeeController.java
+++ src/main/java/com/human_resource/hr_management/v1/controller/EmployeeController.java
@@ -3,12 +3,10 @@ package com.human_resource.hr_management.v1.controller;
 import com.human_resource.hr_management.v1.exceptionHandler.EmployeeNotFoundException;
 import com.human_resource.hr_management.v1.model.Employee;
 import com.human_resource.hr_management.v1.services.EmployeeServiceImpl;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,6 +30,16 @@ public class EmployeeController {
             return ResponseEntity.ok(employeeService.getEmployeeById(employeeId));
         }catch (EmployeeNotFoundException exception){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createEmployee(@RequestBody Employee employee){
+        try{
+            employeeService.createEmployee(employee);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        }catch(DataAccessException exception){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
     }
 
--- src/main/java/com/human_resource/hr_management/v1/services/EmployeeServiceImpl.java
+++ src/main/java/com/human_resource/hr_management/v1/services/EmployeeServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
@@ -31,6 +32,10 @@ public class EmployeeServiceImpl implements EmployeeService{
 
     @Override
     public void createEmployee(Employee employee) {
+        if(employee.getEmployee_id() == null){
+            employee.setEmployee_id(UUID.randomUUID().toString());
+        }
+
         this.employeeDAO.create(employee);
     }
 
